01fd21600a56691863d82ba39b1bebcaf1ae83c7
Techa0600 <138120466+Techa0600@users.noreply.github.com>
2023-07-02 20:26:43 +0700

    Add files via upload

---
+++ Excercise4_Surapat_C.py
@@ -0,0 +1,9 @@
+FE=16.5
+GB=18.2
+ICS=19.5
+CP=12.1
+print("-----Your Score-----")
+print("Foundation English"                ,FE)
+print("General Business"                  ,GB)
+print("Introduction to Computer Systems"  ,ICS)
+print("Computer Programming"              ,CP)
