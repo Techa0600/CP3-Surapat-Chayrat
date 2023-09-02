50b30e0343470765b12117f33c444d0132adb394
Techa0600 <138120466+Techa0600@users.noreply.github.com>
2023-09-02 19:18:22 +0700

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,10 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def times(x,y):
+    print(x*y)
+def division(x,y):
+    print(x/y)
+
+plus(5,2),minus(90,10),times(6,5),division(8,4)
